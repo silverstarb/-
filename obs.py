e8e78d91b09e9b4e3dbd7e907003b4b22123ccbf
silverstarb <silverstarbyeol@daum.net>
2022-04-15 13:51:34 +0900

    .
    .

--- obs.py
+++ obs.py
@@ -11,20 +11,11 @@ async def on_ready():
   await client.change_presence(status=discord.Status.online)
 
   await client.change_presence(activity=discord.Game(name="게임 하는중"))
-  #await client.change_presence(activity=discord.Streaming(name="옵스와아이들 방송중", url='https://www.twitch.tv/startstee'))
+  #await client.change_presence(activity=discord.Streaming(name="스트림 방송중", url='링크'))
   #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="노래 듣는중"))
   #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="영상 시청중"))
   
   print("봇 이름:",client.user.name,"봇 아이디:",client.user.id,"봇 버전:",discord.__version__)
 
-@client.event
-async def on_message(message):
-    if message.content.startswith("안녕"):
-        await message.channel.send("안녕하세요")
 
-@client.event
-async def on_message(message):
-    if message.content.startswith("옵하"):
-        await message.channel.send("{ctx.message.author.mention} 어서오고~")
-
-client.run(os.environ['토큰'])
+client.run(os.environ['token'])
